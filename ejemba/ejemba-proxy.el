bd3614355a785e69d52a0c68129f72eafad53032
Epo Jemba <epo.jemba@kametic.com>
2014-06-20 09:12:49 +0200

    dummy change

--- ejemba/ejemba-proxy.el
+++ ejemba/ejemba-proxy.el
@@ -2,12 +2,12 @@
 ; http://stackoverflow.com/questions/10787087/use-elpa-emacs-behind-proxy-requiring-authentication
 ; http://debbugs.gnu.org/cgi/bugreport.cgi?bug=12069
 
-;(setq url-proxy-services '(("no_proxy" . "inetpsa\\.com")
-;                           ("http" . "http.internetpsa.inetpsa.com:80")
+;(setq url-proxy-services '(("no_proxy" . "host")
+;                           ("http" . "")
 ;                           ("https" . "http.internetpsa.inetpsa.com:80"))
 ;)
 ;
 ;(setq url-http-proxy-basic-auth-storage
-;      (list (list "http.internetpsa.inetpsa.com:80"
+;      (list (list "httphost:80"
 ;		  (cons "Input your LDAP UID !"
-;			(base64-encode-string "e364467:xxxxx")))))
+;			(base64-encode-string "blahblah:xxxxx")))))
